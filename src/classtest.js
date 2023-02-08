1e9eda4c062f60d47a999384adcf54704bf7f504
Anuj07ku <96259050+Anuj07ku@users.noreply.github.com>
2023-02-08 18:13:58 +0530

    1 file modified

--- src/classtest.js
+++ src/classtest.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return <h1>Welcome to Class Component</h1>;
   }
 }
 
-export default App;
+//export default App;
