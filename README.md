37b3e00bf5b2f6f9f607a644f599f3e307995a19
Anuj07ku <96259050+Anuj07ku@users.noreply.github.com>
2023-02-08 15:08:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-AnujK
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-wqmqsk)
---
+++ src/Functionaltest.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../style.css';
+
+const Demo = () => {
+  return <h1>Welcome to Functional Component</h1>;
+};
+
+export default Demo;
