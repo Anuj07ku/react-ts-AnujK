305deaaa6d5ea433cf7374ebbbd722b0ab677b4f
Anuj07ku <96259050+Anuj07ku@users.noreply.github.com>
2023-03-29 14:00:50 +0530

    1 file modified

--- src/formFormik/components/EmployFormFormixYup.js
+++ src/formFormik/components/EmployFormFormixYup.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { withFormik, Field } from 'formik';
 import * as Yup from 'yup';
 
-const EmployFormFormixYup = ({ errors }) => (
+const EmployFormFormixYup = ({ values, errors, touched, isSubmiting }) => (
   <div>
     <h1> Employee Form Formik Validation </h1>
     <form>
@@ -10,14 +10,20 @@ const EmployFormFormixYup = ({ errors }) => (
         Employee Name
         <Field type="text" name="empName" />
         <br />
-        <spam style={{ color: 'red' }}>{errors.empName}</spam>
+        {touched.empName && errors.empName && 
+          <spam style={{ color: 'red' }}>{errors.empName}</spam>
+        }
         <br />
         Employee
-        <Field type="checkbox" name="manager" />
+        <Field type="checkbox" name="manager" checked={values.manager} />
         <br />
-        <spam style={{ color: 'red' }}>{errors.manager}</spam>
+        {errors.manager && 
+          <spam style={{ color: 'red' }}>{errors.manager}</spam>
+        }
         <br />
-        <button type="submit">Submit </button>
+        <button type="submit" disabled={isSubmiting}>
+          Submit{' '}
+        </button>
       </div>
     </form>
   </div>
@@ -36,6 +42,17 @@ const EmployForm = withFormik({
       .min(5, 'Employee Name Must be 5 charector long')
       .required('Employee field can not be blank'),
   }),
+  handleSubmit(values, { resetForm, setSubmitting, setErrors }) {
+    console.log(values);
+    setTimeout(() => {
+      if (values.empName === 'Anuj') {
+        setErrors({ empName: 'Anuj can not be set as Employee Name' });
+      } else {
+        resetForm();
+      }
+      setSubmitting(false);
+    }, 2000);
+  },
 })(EmployFormFormixYup);
 
 export default EmployForm;
